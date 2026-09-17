e0359a5093230e56b0a6351789716603db150061
caiosouza-cs <souza.caio.cs73@gmail.com>
2026-09-17 16:07:06 -0300

    muda cor de botão kkkk

--- resources/views/requerimentos/meusRequerimentos.blade.php
+++ resources/views/requerimentos/meusRequerimentos.blade.php
@@ -68,7 +68,7 @@
                         @endif
                     </td>
                     <td class="py-3 px-6 text-center">
-                        <a href="{{ route('requerimentos.gerar-comprovante', ['id' => $requerimento->id]) }}" class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-1 px-4 rounded-lg">
+                        <a href="{{ route('requerimentos.gerar-comprovante', ['id' => $requerimento->id]) }}" class="bg-green-600 hover:bg-green-700 text-white font-semibold py-1 px-4 rounded-lg">
                             Imprimir comprovante
                         </a>
                     </td>
