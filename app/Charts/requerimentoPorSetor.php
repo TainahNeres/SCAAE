327c53669e220661b847db84b9b55ec3abeee4e7
ualcz <claudeilsonsouzza@gmail.com>
2026-09-19 19:47:02 -0300

    refactor: corrige nome da classe requerimentoPorSetor para RequerimentoPorSetor e ajusta a injeção de dependência no AdminDashboardController

--- app/Charts/requerimentoPorSetor.php
+++ app/Charts/requerimentoPorSetor.php
@@ -5,24 +5,24 @@ namespace App\Charts;
 use ArielMejiaDev\LarapexCharts\LarapexChart;
 use App\Models\Requerimento;
 
-class requerimentoPorSetor
+class RequerimentoPorSetor
 {
     public function build(): \ArielMejiaDev\LarapexCharts\PieChart
-        {
-            $dados = Requerimento::with('setor')
-                ->selectRaw('setor_id, COUNT(*) as total')
-                ->groupBy('setor_id')
-                ->get();
+    {
+        $dados = Requerimento::with('setor')
+            ->selectRaw('setor_id, COUNT(*) as total')
+            ->groupBy('setor_id')
+            ->get();
 
-            $valores = $dados->pluck('total')->toArray();
-            $labels  = $dados->map(function ($item) {
-                return $item->setor->setor_sigla ?? 'Setor ' . $item->setor_id;
-            })->toArray();
+        $valores = $dados->pluck('total')->toArray();
+        $labels  = $dados->map(function ($item) {
+            return $item->setor->setor_sigla ?? 'Setor ' . $item->setor_id;
+        })->toArray();
 
-            return (new \ArielMejiaDev\LarapexCharts\LarapexChart)
-                ->pieChart()
-                ->setTitle('Requerimentos por setor')
-                ->addData($valores)
-                ->setLabels($labels);
-        }
+        return (new LarapexChart)
+            ->pieChart()
+            ->setTitle('Requerimentos por setor')
+            ->addData($valores)
+            ->setLabels($labels);
     }
+}
